1ca8ef6255c80dc24ac7d8d020efdc60440cd698
RodiaGiordano <rodiagiordano93@gmail.com>
2023-08-22 13:26:32 +0200

    feat: logic list bomb

--- script.js
+++ script.js
@@ -10,17 +10,40 @@ const difficulty = document.getElementById('difficulty')
 
 
 
+
 buttonGrid.addEventListener('click', function(){ 
-
+    
     cellContainer.innerHTML = ""; // pulisco la griglia
-
+    
     let classEl; // variabile d'appoggio per le classi
 
+    const bombNumber = 16; //il numero di bombe
+    
     let max = parseInt(difficulty.value) // setto max come il value della option difficulty
-
+    
     if(!max) alert('seleziona una difficoltà')   // controllo che abbia selezionato una difficoltà
+
+
     
-    
+    const whitelistCell = generateProgressiveArray (1, max, 1); // creo una whitelist 
+    console.log("whitelistCell: " + whitelistCell)
+
+    const listBomb = []; // creo un array in cui aggiungero i numeri bomba
+
+    for(let i = 0; i < bombNumber; i++){
+ 
+        const bombEl = whitelistCell.splice((randomNumber(0, (max - listBomb.length), false)), 1); // la bomba è uguale all'elemento rimosso casualmente dalla whitelist, il numero da cui lo splice inizia a contare si adatta alla lunghezza della whitelist
+
+        console.log("genero numero casuale: " + bombEl)
+        console.log("numero tolto: " + bombEl)
+        
+        listBomb.unshift(bombEl); //aggiungo alla lista bombe l'elemento rimosso dalal whitelist
+    }
+   
+    console.log("lista bombe: " + listBomb)
+
+
+
     // istruzioni condizionali per settare classEl
     if(max == 100) classEl = 'box easy' 
     if(max == 81) classEl = 'box medium'
@@ -29,38 +52,13 @@ buttonGrid.addEventListener('click', function(){
     
     for(let i = 1; i <= max; i++){
         
+        const cell = generatedGrid(cellContainer, 'div', classEl, i, listBomb) // creo gli elementi ed inserisco gli attributi
+    }
+})
         
-        const cell = generatedGrid(cellContainer, 'div', classEl, i) // creo gli elementi ed inserisco gli attributi
        
         
-    }
     
-})
-
-
-
-/**
- * modifica l'inner di un object, può avere un counter [da scartare]
- * @param {string} object l'elemento da modificare
- * @param {int} counter possibile inserire un counter
- * 
- *  */
-
-function generatedCell (object, counter) {
-
-
-    object.setAttribute('data-index', counter);
-        
-        
-    let cellShow = object.getAttribute('data-index')
-        
-    object.addEventListener('click', function(){
-            this.classList.add('bg-primary');
-            
-            this.innerText = cellShow;
-            
-        })
-}
 
 
 
@@ -74,15 +72,78 @@ function generatedCell (object, counter) {
  *  */
 
 
-function generatedGrid (container, object, classEl, counter){
+function generatedGrid (container, object, classEl, counter, whitelist){
     
     let cell = document.createElement(object);    
     
     cell.className = classEl
-
-    generatedCell (cell, counter)
+       
+    generatedCell (cell, counter, whitelist)
 
     container.append(cell);
     
     return cell;
 }
+
+
+
+/**
+ * modifica l'inner di un object, può avere un counter [da scartare]
+ * @param {string} object l'elemento da modificare
+ * @param {int} counter possibile inserire un counter
+ * 
+ *  */
+
+function generatedCell (object, counter, whitelist) {
+
+    object.setAttribute('data-index', counter);
+        
+        
+    let cellShow = object.getAttribute('data-index')
+        
+    object.addEventListener('click', function(){
+        if(whitelist.includes(counter)){
+            this.classList.add('bg-danger')
+        }
+            this.classList.add('bg-primary');
+            
+            this.innerText = cellShow;
+            
+        })
+}
+
+
+
+/**
+ * Genera una whitelist
+ * @param {int} from da dove inizia a contare
+ * @param {int} to fino a dove contare
+ * @param {int} step progressione del counter del ciclo
+ * @returns {object} restituisce un array di int
+ *  */
+
+function generateProgressiveArray (from, to, step){
+    const whitelist  = [];
+ 
+    for(let i = from; i <= to; i += step){
+       whitelist.unshift(i);
+    }
+ 
+    return whitelist;
+ }
+ 
+
+/**
+ * funzione che genera un numero random, posso scegliere se includere l'ultimo o no
+ * @param {int} min 
+ * @param {int} max
+ * @param {boolean} [lastIncluded=true] 
+ * @returns {int} il numero randomico generato fra massimo e minimo
+ *  */
+
+function randomNumber(min, max, lastIncluded = true){
+    if(lastIncluded) max++
+    
+    let randomNumber = Math.floor(Math.random() * (max - min) + min);
+    return randomNumber; 
+ }
